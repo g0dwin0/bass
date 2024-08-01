40bc5a2b3fa16d7d748dbada68ad7792bb72fb63
g0dwin0 <21052794@student.hhs.nl>
2024-08-01 21:57:04 +0200

    instruction info: removed execution functionality

---
+++ include/core/instructions/instruction.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "common.hpp"
+#include "registers.hpp"
+// struct InstructionInfo;
+// #include "arm.hpp"
+#include "spdlog/spdlog.h"
+struct ARM7TDMI;
+#include "cpu.hpp"
+
+struct InstructionInfo {
+  u32 opcode = 0;
+  u32 loc    = 0;
+
+  u32 op2;
+  u32 offset;
+
+  u8 Rd      : 4;
+  u8 Rn      : 4;
+  u8 Rs      : 4;
+  u8 Rm      : 4;
+
+  u8 A       : 1;
+  u8 P       : 1;
+  u8 U       : 1;
+  u8 B       : 1;
+  u8 W       : 1;
+  u8 L       : 1;
+  u8 I       : 1;
+  u8 S       : 1;
+  u8 H       : 1;
+
+  Condition condition;
+
+  std::string mnemonic = "";
+
+  void (*func_ptr)(ARM7TDMI& c, InstructionInfo& instr) = NULL;
+};
