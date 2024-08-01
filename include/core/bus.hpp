202cbf4484b2636419037342f519f172852792d2
g0dwin0 <21052794@student.hhs.nl>
2024-08-01 21:58:20 +0200

    * added oam, and palette ram memory to the bus * added ppu device pointer to the bus * added a set8,16,32 function to help do aligned writes to various parts   of memory

--- include/core/bus.hpp
+++ include/core/bus.hpp
@@ -2,24 +2,42 @@
 
 #include "common.hpp"
 #include "pak.hpp"
+#include "ppu.hpp"
 
 struct Bus {
   // memory/devices
-
   std::vector<u8> BIOS;
   std::vector<u8> IWRAM;
   std::vector<u8> EWRAM;
+  std::vector<u8> IO;
+  std::vector<u8> VRAM;
+  std::vector<u8> PALETTE_RAM;
+  std::vector<u8> OAM;
 
-  Bus(): BIOS(0x4000), IWRAM(0x8000), EWRAM(0x40000) { spdlog::debug("created bus"); }
+
+  Bus()
+      : BIOS(0x4000), IWRAM(0x8000), EWRAM(0x40000), IO(0x500), VRAM(0x18000), PALETTE_RAM(0x400), OAM(0x400)  {
+    spdlog::debug("created bus");
+  }
 
   Pak* pak = nullptr;
+  PPU* ppu = nullptr;
 
   u8 read8(u32 address) const;
   u16 read16(u32 address) const;
   u32 read32(u32 address);
 
-  
-  u16 shift_16(std::vector<u8> vec, u32 address);
-  u32 shift_32(std::vector<u8> vec, u32 address);
-  
+  void write8(u32 address, u8 value);
+  void write16(u32 address, u16 value);
+  void write32(u32 address, u32 value);
+
+  void set8(std::vector<u8>& v, u32 address,
+            u8 value);  // TODO: move to utils or smth
+  void set16(std::vector<u8>& v, u32 address,
+             u16 value);  // TODO: move to utils or smth
+  void set32(std::vector<u8>& v, u32 address,
+             u32 value);  // TODO: move to utils or smth
+
+  u16 shift_16(const std::vector<u8>& vec, const u32 address);
+  u32 shift_32(const std::vector<u8>& vec, const u32 address);
 };
--- src/core/bus.cpp
+++ src/core/bus.cpp
@@ -1,19 +1,28 @@
 #include "bus.hpp"
 
-
 enum class RETURN_SIZE { U16, U32 };
 
-u16 Bus::shift_16(std::vector<u8> vec, u32 address) {
+u16 Bus::shift_16(const std::vector<u8>& vec, const u32 address) {
   return (vec.at(address + 1) << 8) + (vec.at(address));
 }
-u32 Bus::shift_32(std::vector<u8> vec, u32 address) {
-  // fmt::println("v: {:#04x}", vec.at(address));
-  // fmt::println("v: {:#04x}", vec.at(address + 1));
-  // fmt::println("v: {:#04x}", vec.at(address + 2));
-  // fmt::println("v: {:#04x}", vec.at(address + 3));
-
+u32 Bus::shift_32(const std::vector<u8>& vec, const u32 address) {
   return ((vec.at(address + 3) << 24) + (vec.at(address + 2) << 16) +
           (vec.at(address + 1) << 8) + (vec.at(address)));
+}
+
+std::array<uint8_t, 2> u16_to_u8s(u32 input) {
+  return {
+      static_cast<uint8_t>((input >> 8) & 0xFF),
+      static_cast<uint8_t>(input & 0xFF)  
+  };
+}
+
+void Bus::set16(std::vector<u8>& vec, u32 address, u16 value) {
+  std::array<u8, 2> v = u16_to_u8s(value);
+
+  for (size_t i = 0; i < 2; i++) {
+    vec[address + i] = v[i];
+  }
 }
 
 u8 Bus::read8(u32 address) const { return address; };
@@ -54,8 +63,58 @@ u32 Bus::read32(u32 address) {
     }
 
     default: {
-      spdlog::warn("unused/oob memory read: {:08X}", address);
+      spdlog::warn("unused/oob memory read: {:#X}", address);
       return 0xFFFFFFFF;
     }
   }
 };
+
+void Bus::write16(const u32 address, u16 value) {
+  SPDLOG_INFO("[W] {:#x} => {:#x}", address, value);
+  switch (address) {
+    case 0x02000000 ... 0x0203FFFF: {
+      // internal work ram read
+      set16(IWRAM, address - 0x02000000, value);
+      break;
+    }
+
+    case 0x03000000 ... 0x03007FFF: {
+      set16(EWRAM, address - 0x03000000, value);
+      break;
+    }
+
+    case 0x04000000 ... 0x4000056: {
+      ppu->handle_write(address, value);
+      break;
+    }
+
+    case 0x4000057 ... 0x040003FE: {
+      // IO
+      set16(IO, address - 0x04000000, value);
+      break;
+    }
+
+    case 0x05000000 ... 0x050003FF: {
+      // BG/OBJ Palette RAM
+      set16(PALETTE_RAM, address - 0x05000000, value);
+      break;
+    }
+
+    case 0x06000000 ... 0x06017FFF: {
+      // VRAM
+      set16(VRAM, address - 0x06000000, value);
+      break;
+    }
+
+    case 0x07000000 ... 0X070003FF: {
+      // OAM
+      set16(VRAM, address - 0x07000000, value);
+      break;
+    }
+
+    default: {
+      spdlog::warn("unused/oob memory write: {:#X}", address);
+      break;
+    }
+  }
+}
