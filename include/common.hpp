dd9f6006b14c59c541ccefa0d7359d5076d02ca7
g0dwin0 <21052794@student.hhs.nl>
2024-08-11 14:12:01 +0200

    common: removed spdlog & fmtlib from common

--- include/common.hpp
+++ include/common.hpp
@@ -3,15 +3,14 @@
 #define CONCAT(x, y) x ## y
 #define EXPAND(x, y) CONCAT(x, y)
 #define RESERVED EXPAND(reserved, __LINE__)
-
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
 #include <cstdlib>
 #include <cstdint>
 #include <cstdio>
 #include <cassert>
 #include <cstdint>
 #include <cstdlib>
-#include "spdlog/spdlog.h"
-#include "fmt/core.h"
+
 
 
 typedef uint8_t     u8;
@@ -19,8 +18,8 @@ typedef uint16_t    u16;
 typedef uint32_t    u32;
 typedef uint64_t    u64;
 
-typedef int8_t     s8;
-typedef int16_t    s16;
-typedef int32_t    s32;
-typedef int64_t    s64;
+typedef int8_t     i8;
+typedef int16_t    i16;
+typedef int32_t    i32;
+typedef int64_t    i64;
 
