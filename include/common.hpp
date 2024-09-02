b173f0f4177a6b5fbc6dbf57f44cd02bca92fca9
g0dwin0 <21052794@student.hhs.nl>
2024-09-02 12:00:24 +0200

    typedef: moved to i to s for signed values

--- include/common.hpp
+++ include/common.hpp
@@ -3,7 +3,9 @@
 #define CONCAT(x, y) x ## y
 #define EXPAND(x, y) CONCAT(x, y)
 #define RESERVED EXPAND(reserved, __LINE__)
+
 #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+
 #include <cstdlib>
 #include <cstdint>
 #include <cstdio>
@@ -18,8 +20,8 @@ typedef uint16_t    u16;
 typedef uint32_t    u32;
 typedef uint64_t    u64;
 
-typedef int8_t     i8;
-typedef int16_t    i16;
-typedef int32_t    i32;
-typedef int64_t    i64;
+typedef int8_t     s8;
+typedef int16_t    s16;
+typedef int32_t    s32;
+typedef int64_t    s64;
 
