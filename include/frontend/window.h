903348636169bb6d28108e7c14e20108de9eff31
g0dwin0 <21052794@student.hhs.nl>
2024-09-10 10:15:27 +0200

    frontend: added controls, spsr view, and rendering
    - added controls (see default controls in window.h)
    - added settings structure into frontend (they do not save, yet.)
    - added menu to rebind controls
    
    - added view of all spsr in the debugger
    
    - sped up rendering mode 3/4 by stepping CPU before calling render
      function, instead of stepping 1 cycle, then rendering.

--- include/frontend/window.h
+++ include/frontend/window.h
@@ -2,24 +2,51 @@
 
 #include <SDL2/SDL.h>
 
+#include <unordered_map>
+
 #include "bass.hpp"
 #include "common.hpp"
 #include "imgui.h"
 
-
 struct State {
   SDL_Texture* ppu_texture = nullptr;
-  u32* frame_buf_ptr = nullptr;
-  
+  u32* frame_buf_ptr       = nullptr;
+
   const u8* keyboardState = SDL_GetKeyboardState(nullptr);
   bool running            = true;
 
-  bool debugger_open = true;
-  bool cpu_info_open = true;
-  bool ppu_info_open = true;
-  
-  ImGuiIO* io        = nullptr;
+  bool debugger_open        = true;
+  bool cpu_info_open        = true;
+  bool ppu_info_open        = true;
+  bool controls_window_open = true;
+
+  ImGuiIO* io = nullptr;
 };
+
+enum KEY { A, B, SELECT, START, RIGHT, LEFT, UP, DOWN, L, R, NONE = 0xFF };
+
+struct Keymap_State {
+  SDL_Scancode current_key;
+  bool being_remapped;
+};
+struct Settings {
+  struct Keybinds {
+    std::unordered_map<KEY, Keymap_State> control_map = {
+        {     KEY::A,         {SDL_SCANCODE_Z, false}},
+        {     KEY::B,         {SDL_SCANCODE_X, false}},
+        {     KEY::L,         {SDL_SCANCODE_A, false}},
+        {     KEY::R,         {SDL_SCANCODE_S, false}},
+        {KEY::SELECT, {SDL_SCANCODE_BACKSPACE, false}},
+        { KEY::START,    {SDL_SCANCODE_RETURN, false}},
+        { KEY::RIGHT,     {SDL_SCANCODE_RIGHT, false}},
+        {  KEY::LEFT,      {SDL_SCANCODE_LEFT, false}},
+        {    KEY::UP,        {SDL_SCANCODE_UP, false}},
+        {  KEY::DOWN,      {SDL_SCANCODE_DOWN, false}},
+    };
+
+  } keybinds;
+};
+
 struct Frontend {
   SDL_Window* window;
   SDL_Renderer* renderer;
@@ -37,6 +64,22 @@ struct Frontend {
   void show_debugger();
   void show_cpu_info();
   void show_ppu_info();
-  
+  void show_controls_menu(bool*);
+
   explicit Frontend(Bass*);
+
+  Settings settings;
+};
+
+const inline std::unordered_map<KEY, std::string> buttons = {
+    {     A,      "A"},
+    {     B,      "B"},
+    {     L,      "L"},
+    {     R,      "R"},
+    { START,  "START"},
+    {SELECT, "SELECT"},
+    { RIGHT,  "RIGHT"},
+    {  LEFT,   "LEFT"},
+    {    UP,     "UP"},
+    {  DOWN,   "DOWN"},
 };
--- src/frontend/window.cpp
+++ src/frontend/window.cpp
@@ -2,6 +2,7 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_timer.h>
 
 #include "cpu.hpp"
 #include "imgui.h"
@@ -15,10 +16,7 @@ static MemoryEditor editor_instance;
 
 void Frontend::handle_events() {
   SDL_Event event;
-
   while (SDL_PollEvent(&event)) {
-    ImGui_ImplSDL2_ProcessEvent(&event);
-
     switch (event.type) {
       case SDL_QUIT: {
         SPDLOG_INFO("SDL QUIT CALLED");
@@ -28,64 +26,78 @@ void Frontend::handle_events() {
       }
 
       case SDL_KEYDOWN: {
-        // const u8* keyboardState = SDL_GetKeyboardState(nullptr);
-
-        // fmt::println("key pressed: {}",
-        //              SDL_GetKeyName(SDL_GetKeyFromScancode(event.key.keysym.scancode)));
+        if (state.keyboardState[settings.keybinds.control_map.at(RIGHT).current_key]) { bass->bus.keypad_input.KEYINPUT.RIGHT = 0; }
+        if (state.keyboardState[settings.keybinds.control_map.at(LEFT).current_key]) { bass->bus.keypad_input.KEYINPUT.LEFT = 0; }
+        if (state.keyboardState[settings.keybinds.control_map.at(UP).current_key]) { bass->bus.keypad_input.KEYINPUT.UP = 0; }
+        if (state.keyboardState[settings.keybinds.control_map.at(DOWN).current_key]) { bass->bus.keypad_input.KEYINPUT.DOWN = 0; }
+        if (state.keyboardState[settings.keybinds.control_map.at(A).current_key]) { bass->bus.keypad_input.KEYINPUT.A = 0; }
+        if (state.keyboardState[settings.keybinds.control_map.at(B).current_key]) { bass->bus.keypad_input.KEYINPUT.B = 0; }
+        if (state.keyboardState[settings.keybinds.control_map.at(L).current_key]) { bass->bus.keypad_input.KEYINPUT.L = 0; }
+        if (state.keyboardState[settings.keybinds.control_map.at(R).current_key]) { bass->bus.keypad_input.KEYINPUT.R = 0; }
+        if (state.keyboardState[settings.keybinds.control_map.at(SELECT).current_key]) { bass->bus.keypad_input.KEYINPUT.SELECT = 0; }
+        if (state.keyboardState[settings.keybinds.control_map.at(START).current_key]) { bass->bus.keypad_input.KEYINPUT.START = 0; }
 
         break;
       }
 
       case SDL_KEYUP: {
-        // const u8* keyboardState = SDL_GetKeyboardState(nullptr);
-        // fmt::println("key released: {}",
-        //              SDL_GetKeyName(SDL_GetKeyFromScancode(event.key.keysym.scancode)));
+        if (!state.keyboardState[settings.keybinds.control_map.at(RIGHT).current_key]) { bass->bus.keypad_input.KEYINPUT.RIGHT = 1; }
+        if (!state.keyboardState[settings.keybinds.control_map.at(LEFT).current_key]) { bass->bus.keypad_input.KEYINPUT.LEFT = 1; }
+        if (!state.keyboardState[settings.keybinds.control_map.at(UP).current_key]) { bass->bus.keypad_input.KEYINPUT.UP = 1; }
+        if (!state.keyboardState[settings.keybinds.control_map.at(DOWN).current_key]) { bass->bus.keypad_input.KEYINPUT.DOWN = 1; }
+        if (!state.keyboardState[settings.keybinds.control_map.at(A).current_key]) { bass->bus.keypad_input.KEYINPUT.A = 1; }
+        if (!state.keyboardState[settings.keybinds.control_map.at(B).current_key]) { bass->bus.keypad_input.KEYINPUT.B = 1; }
+        if (!state.keyboardState[settings.keybinds.control_map.at(L).current_key]) { bass->bus.keypad_input.KEYINPUT.L = 1; }
+        if (!state.keyboardState[settings.keybinds.control_map.at(R).current_key]) { bass->bus.keypad_input.KEYINPUT.R = 1; }
+        if (!state.keyboardState[settings.keybinds.control_map.at(SELECT).current_key]) { bass->bus.keypad_input.KEYINPUT.SELECT = 1; }
+        if (!state.keyboardState[settings.keybinds.control_map.at(START).current_key]) { bass->bus.keypad_input.KEYINPUT.START = 1; }
 
         break;
       }
     }
+    ImGui_ImplSDL2_ProcessEvent(&event);
   }
 }
 
 void Frontend::show_debugger() {
   ImGui::Begin("DEBUGGER", &state.debugger_open, 0);
-  const char* regions[] = { // TODO: add mirrors & other pak wait states
+  const char* regions[] = {
       "Region: [0x00000000 - 0x00003FFF] BIOS",
       "Region: [0x02000000 - 0x0203FFFF] EWRAM",
       "Region: [0x03000000 - 0x03007FFF] IWRAM",
-      "Region: [0x04000000 - 0x040003FE] IO",
+      // "Region: [0x04000000 - 0x040003FE] IO",
       "Region: [0x05000000 - 0x050003FF] BG/OBJ Palette RAM",
       "Region: [0x06000000 - 0x06017FFF] VRAM",
       "Region: [0x07000000 - 0x070003FF] OAM",
-      "Region: [0x08000000 - 0x09FFFFFF] Game Pak ROM (WS0)",
+      "Region: [0x08000000 - 0x09FFFFFF] Game Pak ROM",
       "Region: [0x0E000000 - 0x0E00FFFF] Game Pak SRAM",
   };
+  // ImGui::Text("KEYCNT: {:%#010X}", bass->bus.keypad.KEYINPUT.value);
+  // ImGui::Text("KEYCNT: {:%10s}", +bass->bus.keypad.KEYINPUT.trv_ptr);
+  // ImGui::Text("keys pressed: %s", bass->bus.keypad.KEYINPUT.get_keys_pressed().c_str());
+
+  // ImGui::Text("Cycles Consumed: UNIMPL");
   const std::vector<u8>* memory_partitions[] = {
-    &bass->bus.BIOS,
-    &bass->bus.EWRAM,
-    &bass->bus.IWRAM,
-    &bass->bus.IO,
-    &bass->bus.PALETTE_RAM,
-    &bass->bus.VRAM,
-    &bass->bus.OAM,
-    &bass->bus.pak->data,
-    &bass->bus.SRAM,
-    
+      &bass->bus.BIOS,
+      &bass->bus.EWRAM,
+      &bass->bus.IWRAM,
+      // &bass->bus.IO,
+      &bass->bus.PALETTE_RAM,
+      &bass->bus.VRAM,
+      &bass->bus.OAM,
+      &bass->bus.pak->data,
+      &bass->bus.SRAM,
+
   };
 
   static int SelectedItem = 0;
 
-
-  if(ImGui::Combo("Regions", &SelectedItem, regions, IM_ARRAYSIZE(regions))) {
-    SPDLOG_DEBUG("switched to: {}", regions[SelectedItem]);
-  }
-  // editor_instance.HighlightFn
+  if (ImGui::Combo("Regions", &SelectedItem, regions, IM_ARRAYSIZE(regions))) { SPDLOG_DEBUG("switched to: {}", regions[SelectedItem]); }
   editor_instance.OptShowAscii = false;
-  editor_instance.ReadOnly = true;
+  // editor_instance.ReadOnly = true;
+
   editor_instance.DrawContents((void*)memory_partitions[SelectedItem]->data(), memory_partitions[SelectedItem]->size());
 
-
-  ImGui::Text("Cycles Consumed: UNIMPL");
   ImGui::End();
 }
 
@@ -94,10 +106,16 @@ void Frontend::show_cpu_info() {
   ImGui::Columns(4, "Registers");
   for (u8 i = 0; i < 16; i++) {
     if (i % 4 == 0 && i != 0) ImGui::NextColumn();
-    ImGui::Text("%s",
-                fmt::format("r{:d}: {:#010x}", i, bass->cpu.regs.r[i]).c_str());
+    ImGui::Text("%s", fmt::format("r{:d}: {:#010x}", i, bass->cpu.regs.r[i]).c_str());
   }
   ImGui::Columns(1);
+  ImGui::Separator();
+  ImGui::Text("%s", fmt::format("SPSR_fiq: {:#010x}", bass->cpu.regs.SPSR_fiq).c_str());
+  ImGui::Text("%s", fmt::format("SPSR_irq: {:#010x}", bass->cpu.regs.SPSR_irq).c_str());
+  ImGui::Text("%s", fmt::format("SPSR_svc: {:#010x}", bass->cpu.regs.SPSR_svc).c_str());
+  ImGui::Text("%s", fmt::format("SPSR_abt: {:#010x}", bass->cpu.regs.SPSR_abt).c_str());
+  ImGui::Text("%s", fmt::format("SPSR_und: {:#010x}", bass->cpu.regs.SPSR_und).c_str());
+  
   ImGui::Separator();
   bool zero     = bass->cpu.regs.CPSR.ZERO_FLAG;
   bool negative = bass->cpu.regs.CPSR.SIGN_FLAG;
@@ -118,13 +136,11 @@ void Frontend::show_cpu_info() {
 
   ImGui::BeginDisabled(true);
   ImGui::Text("CPSR: %#010x", bass->cpu.regs.CPSR.value);
-  ImGui::Text("SPSR: %#010x",
-              bass->cpu.regs.get_spsr(bass->cpu.regs.CPSR.MODE_BITS));
+  ImGui::Text("SPSR: %#010x", bass->cpu.regs.get_spsr(bass->cpu.regs.CPSR.MODE_BITS));
 
   ImGui::Text("CPU MODE: %s", bass->cpu.regs.CPSR.STATE_BIT ? "THUMB" : "ARM");
-  ImGui::Text(
-      "OPERATING MODE: %s",
-      fmt::format("{}", mode_map.at(bass->cpu.regs.CPSR.MODE_BITS)).c_str());
+  ImGui::Text("OPERATING MODE: %s", fmt::format("{}", mode_map.at(bass->cpu.regs.CPSR.MODE_BITS)).c_str());
+  ImGui::Text("CYCLES ELAPSED: %s", fmt::format("{}", bass->bus.cycles_elapsed).c_str());
 
   ImGui::EndDisabled();
 
@@ -132,6 +148,7 @@ void Frontend::show_cpu_info() {
 
   ImGui::Text("FIQ DISABLED: 0x%02x\n", bass->cpu.regs.CPSR.FIQ_DISABLE);
   ImGui::Text("IRQ DISABLED: 0x%02x\n", bass->cpu.regs.CPSR.IRQ_DISABLE);
+  ImGui::Text("KEYINPUT: 0x%02x\n", bass->bus.keypad_input.KEYINPUT.v);
 
   if (ImGui::Button("STEP")) { bass->cpu.step(); }
   ImGui::End();
@@ -139,11 +156,64 @@ void Frontend::show_cpu_info() {
 void Frontend::show_ppu_info() {
   ImGui::Begin("PPU INFO", &state.cpu_info_open, 0);
 
-  ImGui::Text("BG MODE: %#010x", bass->ppu.DISPCNT.BG_MODE);
-  if (ImGui::Button("Set VBLANK")) { bass->ppu.DISPSTAT.VBLANK_FLAG = 1; }
-  if (ImGui::Button("Reset VBLANK")) { bass->ppu.DISPSTAT.VBLANK_FLAG = 0; }
+  ImGui::Text("BG MODE: %#010x", bass->bus.display_fields.DISPCNT.BG_MODE);
+  if (ImGui::Button("Set VBLANK")) { bass->bus.display_fields.DISPSTAT.VBLANK_FLAG = 1; }
+  if (ImGui::Button("Reset VBLANK")) { bass->bus.display_fields.DISPSTAT.VBLANK_FLAG = 0; }
   if (ImGui::Button("Draw")) { bass->ppu.draw(); }
   ImGui::Separator();
+  ImGui::End();
+}
+
+void Frontend::show_controls_menu(bool* p_open) {
+  ImGui::Begin("Controls", p_open);
+  ImGui::Text("Remap your controls here.");
+  ImGui::Separator();
+
+  bool invalid_keybind = false;
+
+  for (SDL_Scancode current_scancode = SDL_SCANCODE_A; current_scancode < SDL_SCANCODE_AUDIOFASTFORWARD; current_scancode = (SDL_Scancode)(current_scancode + 1)) {
+    if (!state.keyboardState[current_scancode]) continue;
+    ImGui::SameLine();
+
+    // refactor: hard to read; use structured bindings
+    for (auto& current_key : settings.keybinds.control_map) {
+      // Actual key represents the key on GBA (L, R, START etc...);
+      auto& actual_key = current_key.first;
+
+      // This indicates the scancode (the key on your keyboard) of the corresponding actual key (key on the GBA)
+      auto& [scancode, being_remapped] = current_key.second;
+
+      // If button is set to be remapped, remap with next key input
+      if (being_remapped) {
+        being_remapped = false;
+        for (auto& [diff_key, keymap_state] : settings.keybinds.control_map) {
+          if (diff_key != actual_key && keymap_state.current_key == current_scancode) { invalid_keybind = true; }
+        }
+        if (!invalid_keybind) {
+          scancode       = current_scancode;
+          being_remapped = false;  // unset remap flag
+        }
+      }
+    }
+  }
+
+  ImGui::Separator();
+  ImGui::Spacing();
+
+  ImGui::SameLine();
+
+  for (auto& entry : settings.keybinds.control_map) {
+    ImGui::PushID(&entry);
+    auto& [key, keymap_state]        = entry;
+    auto& [scancode, being_remapped] = keymap_state;
+
+    ImGui::Text("%s: ", buttons.at(key).c_str());
+    ImGui::SameLine();
+    if (ImGui::Button(being_remapped ? "Waiting for input..." : SDL_GetScancodeName(scancode))) {
+      being_remapped = true;  // Being remapped
+    }
+    ImGui::PopID();
+  }
   ImGui::End();
 }
 
@@ -152,8 +222,7 @@ void Frontend::show_menubar() {
     ImGui::Separator();
     if (ImGui::BeginMenu("File")) {
       if (ImGui::MenuItem("Load ROM")) {
-        auto path = tinyfd_openFileDialog("Load ROM", "./", 2, patterns,
-                                          "Gameboy Advance ROMs", 0);
+        auto path = tinyfd_openFileDialog("Load ROM", "./", 2, patterns, "Gameboy Advance ROMs", 0);
         if (path != nullptr) {
           // TODO: add cart loading logic
         }
@@ -182,30 +251,13 @@ void Frontend::render_frame() {
   ImGui_ImplSDLRenderer2_NewFrame();
   ImGui_ImplSDL2_NewFrame();
   ImGui::NewFrame();
-  // show_menubar();
-  // bool t = true;
-  // ImGui::ShowDemoWindow(&t);
   show_cpu_info();
   show_ppu_info();
   show_debugger();
 
-  // iwram_view.DrawWindow("IWRAM View", bass->bus.IWRAM.data(),
-  //                       bass->bus.IWRAM.size());
-
-  // vram_view.DrawWindow("VRAM View", bass->bus.VRAM.data(),
-  //                      bass->bus.VRAM.size());
-
-  // palette_view.DrawWindow("Palette RAM View", bass->bus.PALETTE_RAM.data(),
-  //                         bass->bus.PALETTE_RAM.size());
-  // pak_view.DrawWindow("Pak View", bass->bus.pak->data.data(),
-  //                     bass->bus.pak->data.size());
-  // frame_buffer_view.DrawWindow("Frame Buffer View", bass->bus.ppu->frame_buffer,
-  //                              38400);
-
   // Rendering
   ImGui::Render();
-  SDL_RenderSetScale(renderer, state.io->DisplayFramebufferScale.x,
-                     state.io->DisplayFramebufferScale.y);
+  SDL_RenderSetScale(renderer, state.io->DisplayFramebufferScale.x, state.io->DisplayFramebufferScale.y);
 
   SDL_SetRenderTarget(renderer, NULL);
   SDL_RenderClear(renderer);
@@ -223,18 +275,11 @@ Frontend::Frontend(Bass* c) {
 
   SPDLOG_DEBUG("constructed frontend with instance pointer");
 
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER) !=
-      0) {
-    fmt::println("ERROR: failed initialize SDL");
-  }
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER) != 0) { fmt::println("ERROR: failed initialize SDL"); }
 
-  auto window_flags =
-      (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE |
-                        SDL_WINDOW_ALLOW_HIGHDPI);
+  auto window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
 
-  this->window =
-      SDL_CreateWindow("bass", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       240 * 5, 160 * 5, window_flags);
+  this->window   = SDL_CreateWindow("bass", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 240 * 5, 160 * 5, window_flags);
   this->renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
   // Setup Dear ImGui context
@@ -243,20 +288,12 @@ Frontend::Frontend(Bass* c) {
   ImGuiIO& io = ImGui::GetIO();
   (void)io;
 
-  // io.ConfigFlags |=
-  //     ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-  io.ConfigFlags |=
-      ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
   this->state.io = &io;
 
   ImGui::StyleColorsDark();
 
-  this->state.ppu_texture = SDL_CreateTexture(
-      renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_TARGET, 240, 160);
-
-  // this->state.ppu_texture = SDL_CreateTexture(
-  //     renderer, SDL_PIXELFORMAT_BGR555, SDL_TEXTUREACCESS_TARGET, 240, 160);
-
+  this->state.ppu_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_TARGET, 240, 160);
+  
   ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
   ImGui_ImplSDLRenderer2_Init(renderer);
 }
