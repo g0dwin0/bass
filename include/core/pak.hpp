98e410de2fce94fe06dd45a182289598f2c0246e
Godwin <godwin@amegaku.re>
2025-08-03 17:21:51 +0200

    save: added cart save type identification

--- include/core/pak.hpp
+++ include/core/pak.hpp
@@ -1,65 +1,82 @@
 #pragma once
-#include "spdlog/sinks/stdout_color_sinks.h"
-
-static constexpr uint8_t ENTRY_POINT_LEN   = 4;
-static constexpr uint8_t BITMAP_SIZE       = 156;
-static constexpr uint8_t GAME_TITLE_LENGTH = 12;
-static constexpr uint8_t GAME_CODE_LENGTH  = 4;
-static constexpr uint8_t MAKER_CODE_LENGTH = 2;
-
+#include <regex>
 #include <vector>
 
 #include "common.hpp"
+#include "common/defs.hpp"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+static constexpr u8 BITMAP_SIZE       = 156;
+static constexpr u8 GAME_TITLE_LENGTH = 12;
+static constexpr u8 GAME_CODE_LENGTH  = 4;
+static constexpr u8 MAKER_CODE_LENGTH = 2;
+static constexpr u32 MAX_ROM_SIZE     = 32 * 1024 * 1024;
+
+enum CartridgeType { EEPROM, SRAM, FLASH, FLASH512, FLASH1M, UNKNOWN };
+
+// thank you, dillon! https://dillonbeliveau.com/2020/06/05/GBA-FLASH.html
+const std::unordered_map<CartridgeType, std::regex> cart_type_lookup_regex_map = {
+    {  EEPROM,   std::regex("EEPROM_V\\d\\d\\d")},
+    {    SRAM,     std::regex("SRAM_V\\d\\d\\d")},
+    {   FLASH,    std::regex("FLASH_V\\d\\d\\d")},
+    {FLASH512, std::regex("FLASH512_V\\d\\d\\d")},
+    { FLASH1M,  std::regex("FLASH1M_V\\d\\d\\d")},
+};
+
+const std::unordered_map<CartridgeType, std::string> cart_type_lookup_map = {
+    {  EEPROM,       "EEPROM"},
+    {    SRAM,         "SRAM"},
+    {   FLASH,        "FLASH"},
+    {FLASH512,     "FLASH512"},
+    { FLASH1M,      "FLASH1M"},
+    { UNKNOWN, "UNKNOWN/NONE"},
+};
 
 struct Pak {
-  Pak() : data(0x2000000) {};
+  Pak() : data(MAX_ROM_SIZE) {};
 
   std::shared_ptr<spdlog::logger> pak_logger = spdlog::stdout_color_mt("PAK");
 
   std::vector<u8> data;
+  struct {
+    union {
+      u8 header_bytes[192];
+      struct {
+        // 32bit ARM branch opcode
+        u32 entry_point;
 
-  union {
-    u8 header_bytes[192];
-    struct {
-      // 32bit ARM branch opcode
-      u8 entry_point[ENTRY_POINT_LEN];
+        u8 logo[BITMAP_SIZE];
 
-      // compressed bitmap, required.
-      u8 logo[BITMAP_SIZE];
+        char game_title[GAME_TITLE_LENGTH];
 
-      // game title in ASCII (12 characters)
-      char game_title[GAME_TITLE_LENGTH];
+        char game_code[GAME_CODE_LENGTH];
 
-      // game code (shorthand 4 characters)
-      char game_code[GAME_CODE_LENGTH];
+        char maker_code[MAKER_CODE_LENGTH];
 
-      // maker code (shorthand 2 characters)
-      char maker_code[MAKER_CODE_LENGTH];
+        // fixed value (0x96 for valid roms -- probably breaks user-made ROMs, ignore)
+        u8 fixed_value;
 
-      // fixed value (0x96 for valid roms)
-      u8 RESERVED;
+        u8 unit_code;
 
-      // unit device code (0=GBA)
-      u8 unit_code;
+        u8 device_type;
 
-      // DEVICE TYPE
-      u8 device_type;
+        // should be 7 zeroes on valid roms
+        u8 RESERVED[7];
 
-      // should be 7 zeroes on valid roms
-      u8 RESERVED[7];
+        u8 software_version;
 
-      // software/game version number
-      u8 software_version;
+        // complement check / header checksum
+        u8 complement_check;
 
-      // complement check / header checksum
-      u8 complement_check;
-
-      // reserved
-      u8 RESERVED[2];
+        u8 RESERVED[2];
+      };
     };
+
+    CartridgeType cartridge_save_type = UNKNOWN;
 
   } info;
 
   void load_data(std::vector<u8>&);
-  void log_cart_info();
+  void log_cart_info() const;
+  CartridgeType get_cartridge_type() const;
 };
--- src/core/pak.cpp
+++ src/core/pak.cpp
@@ -1,22 +1,39 @@
 #include "core/pak.hpp"
+
+#include <regex>
+
 #include "common.hpp"
 
-void Pak::load_data(std::vector<u8>& f) { // REFACTOR: header bytes are in data too... use that instead of loading it in two places
-  for (size_t i = 0; i < f.size(); ++i) {
-    data.at(i) = f.at(i);
-  }
+void Pak::load_data(std::vector<u8>& file_vector) {
+  file_vector.resize(MAX_ROM_SIZE);
+
+  data = file_vector;
 
   for (size_t i = 0; i < 192; i++) {
-    info.header_bytes[i] = f.at(i);
+    info.header_bytes[i] = file_vector.at(i);
   }
-  log_cart_info();
 
-  // fmt::println("pak sz after loading rom: {:#x}", data.size());
+  info.cartridge_save_type = get_cartridge_type();
+
+  log_cart_info();
 };
 
-void Pak::log_cart_info() {
+CartridgeType Pak::get_cartridge_type() const {
+  std::string rom_str(reinterpret_cast<const char*>(data.data()), data.size());
+
+  for (const auto& [type, regex] : cart_type_lookup_regex_map) {
+    if (std::regex_search(rom_str, regex)) {
+      return type;
+    };
+  }
+
+  return UNKNOWN;
+}
+
+void Pak::log_cart_info() const {
   pak_logger->info("GAME TITLE:       {:.12}", info.game_title);
   pak_logger->info("GAME CODE:        {:.4}", info.game_code);
   pak_logger->info("MAKER CODE:       {:.2}", info.maker_code);
   pak_logger->info("SOFTWARE VERSION: {}", info.software_version);
+  pak_logger->info("CARTRIDGE SAVE TYPE: {}", cart_type_lookup_map.at(info.cartridge_save_type));
 }
