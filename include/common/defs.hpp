45057adf01366a4b56ca8c20e6e8566d6ef4eff9
g0dwin0 <21052794@student.hhs.nl>
2025-10-05 06:32:44 +0200

    add cstddef

--- include/common/defs.hpp
+++ include/common/defs.hpp
@@ -1,7 +1,7 @@
 #pragma once
+#include <cstddef>
 #include <cstdint>
 #include <limits>
-
 using u8  = uint8_t;
 using u16 = uint16_t;
 using u32 = uint32_t;
@@ -12,7 +12,7 @@ using i16 = int16_t;
 using i32 = int32_t;
 using i64 = int64_t;
 
-static constexpr auto U8_MAX = std::numeric_limits<u8>::max();
+static constexpr auto U8_MAX  = std::numeric_limits<u8>::max();
 static constexpr auto U16_MAX = std::numeric_limits<u16>::max();
 static constexpr auto U32_MAX = std::numeric_limits<u32>::max();
 static constexpr auto U64_MAX = std::numeric_limits<u64>::max();
