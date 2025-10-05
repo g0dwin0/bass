925242de9ae1fec02a022ea84f7608d8e608bbbc
g0dwin0 <21052794@student.hhs.nl>
2025-10-05 06:32:24 +0200

    agb: added timer arr

--- include/core/agb.hpp
+++ include/core/agb.hpp
@@ -5,25 +5,28 @@
 #include "core/dma.hpp"
 #include "core/pak.hpp"
 #include "core/ppu.hpp"
+#include "timer.hpp"
 
 struct AGB {
   ARM7TDMI cpu = {};
   PPU ppu      = {};
   Bus bus      = {};
   Pak pak      = {};
+  static u64 cycles_elapsed;
 
   // Stopwatch stopwatch;
 
   std::array<DMAContext*, 4> dma_channels;
+  std::array<Timer, 4> timers;
 
   std::atomic<bool> active = true;
 
   AGB();
   ~AGB();
 
-  void set_ppu_interrupts();
-  void check_for_dma();
+  void check_for_dma() const;
+  void tick_timers(u64 cycles);
   void system_loop();
 
-  bool start_timing_cond_met(DMAContext* ch);
+  bool start_timing_cond_met(DMAContext* ch) const;
 };
