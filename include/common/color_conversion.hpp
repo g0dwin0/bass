65d9c0a3214b15c834619f8cb557003710918486
g0dwin0 <21052794@student.hhs.nl>
2025-11-03 03:50:50 +0100

    color correction

--- include/common/color_conversion.hpp
+++ include/common/color_conversion.hpp
@@ -1,20 +1,26 @@
 #pragma once
 #include <array>
+#include <cmath>
 
 #include "defs.hpp"
 
-inline constexpr u8 convert5to8(uint8_t value5) { return (value5 * 255 + 15) / 31; }
+constexpr u8 convert5to8(uint8_t value5) { return (value5 * 255 + 15) / 31; }
 
-inline constexpr u32 BGR555toRGB888(u8 msb, u8 lsb) {
+constexpr u32 BGR555toRGB888(u8 msb, u8 lsb) {
   const u16 bgr555 = (static_cast<u16>(msb) << 8) | lsb;
 
-  const u8 blue5  = (bgr555 & 0x1F);
-  const u8 green5 = (bgr555 >> 5) & 0x1F;
-  const u8 red5   = (bgr555 >> 10) & 0x1F;
+  u8 B = (bgr555 & 0x1F);
+  u8 G = (bgr555 >> 5) & 0x1F;
+  u8 R = (bgr555 >> 10) & 0x1F;
 
-  const u8 b = convert5to8(blue5);
-  const u8 g = convert5to8(green5);
-  const u8 r = convert5to8(red5);
+  double lcdGamma = 4.0, outGamma = 2.2;
+  double lb = pow(B / 31.0, lcdGamma);
+  double lg = pow(G / 31.0, lcdGamma);
+  double lr = pow(R / 31.0, lcdGamma);
+
+  u8 r = static_cast<u8>(pow((0 * lb + 50 * lg + 255 * lr) / 255, 1 / outGamma) * (255 * 255 / 280));
+  u8 g = static_cast<u8>(pow((30 * lb + 230 * lg + 10 * lr) / 255, 1 / outGamma) * (255 * 255 / 280));
+  u8 b = static_cast<u8>(pow((220 * lb + 10 * lg + 50 * lr) / 255, 1 / outGamma) * (255 * 255 / 280));
 
   return (static_cast<u32>(r) << 16) | (static_cast<u32>(g) << 8) | b;
 }
